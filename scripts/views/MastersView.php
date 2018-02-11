108dd0deebec7eb24686b2c9ae81548e3dbec43f
MaksymMaksymov <maksymmaksymov@gmail.com>
2018-02-11 18:51:38 +0200

    Fixed info for persons

--- scripts/views/MastersView.php
+++ scripts/views/MastersView.php
@@ -12,7 +12,7 @@
   <?php
     include_once("View.php");
     if (isset($get_info) && $get_info -> masters != null) {
-      $arr_classes = array(array("master","sort"),"job_type","race",array("practic","sort"),array("cosmetic","sort"),array("city","sort"),array("integrity","sort"),array("power","sort"),array("power_in","sort"),array("power_out","sort"),"job",array("positive","sort"),array("negative","sort"));
+      $arr_classes = array(array("master","sort"),array("job_type","sort"),array("race","sort"),array("practic","sort"),array("cosmetic","sort"),array("city","sort"),array("integrity","sort"),array("power","sort"),array("power_in","sort"),array("power_out","sort"),"job",array("positive","sort"),array("negative","sort"));
       $arr_headers = array("Мастер","Профессия","Раса","Практическая","Косметическая","Город",array("type" => "img", "src" => "house.png", "alt" => "Целостность строения"),array("type" => "img", "src" => "power_ui.png", "alt" => "% влияния среди Мастеров в городе"),array("type" => "img", "src" => "power_in_ui.png", "alt" => "Влияние от Ближнего круга"),array("type" => "img", "src" => "power_out_ui.png", "alt" => "Влияние от Народа"),array("type" => "img", "src" => "job_ui.png", "alt" => "Проект"),array("type" => "img", "src" => "good.png", "alt" => "Остаток влияния соратникам"),array("type" => "img", "src" => "bad.png", "alt" => "Остаток влияния противникам"));
       $table_name = "master_table";
       $arr_out_masters = array();
