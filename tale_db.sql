36cc30915aa566a79d01f12b79306cc1411bfdaa
MaksymMaksymov <maksymmaksymov@gmail.com>
2018-02-20 21:28:35 +0200

    sql updated

--- tale_db.sql
+++ tale_db.sql
@@ -3,11 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Фев 20 2018 г., 19:00
+-- Время создания: Фев 20 2018 г., 20:28
 -- Версия сервера: 10.1.26-MariaDB
 -- Версия PHP: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,126 +19,126 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: tale_db
+-- База данных: `tale_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы heroes
+-- Структура таблицы `heroes`
 --
 
-CREATE TABLE heroes (
-  id int(11) NOT NULL,
-  angel_name varchar(250) COLLATE utf8_bin NOT NULL,
-  name varchar(250) COLLATE utf8_bin NOT NULL,
-  might int(11) DEFAULT NULL,
-  can_affect_game tinyint(1) DEFAULT NULL,
-  last_visit datetime DEFAULT NULL,
-  alive tinyint(1) DEFAULT NULL,
-  clan_name varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  clan_id int(11) DEFAULT NULL,
-  gender int(11) DEFAULT NULL,
-  race int(11) DEFAULT NULL,
-  level int(11) DEFAULT NULL,
-  exp int(11) DEFAULT NULL,
-  power int(11) DEFAULT NULL,
-  companion varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  peacefulness int(11) DEFAULT NULL,
-  peacefulness_verbose varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  honor int(11) DEFAULT NULL,
-  honor_verbose varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  money int(11) DEFAULT NULL,
-  strength int(11) DEFAULT NULL,
-  physic int(11) DEFAULT NULL,
-  magic int(11) DEFAULT NULL,
-  lvl_equip int(11) DEFAULT NULL,
-  lvl_equip_title varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  avg_equip int(11) DEFAULT NULL,
-  speed float DEFAULT NULL,
-  initiative float DEFAULT NULL,
-  position varchar(10) COLLATE utf8_bin DEFAULT NULL,
-  quest varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  quest_list varchar(500) COLLATE utf8_bin DEFAULT NULL
+CREATE TABLE `heroes` (
+  `id` int(11) NOT NULL,
+  `angel_name` varchar(250) COLLATE utf8_bin NOT NULL,
+  `name` varchar(250) COLLATE utf8_bin NOT NULL,
+  `might` int(11) DEFAULT NULL,
+  `can_affect_game` tinyint(1) DEFAULT NULL,
+  `last_visit` datetime DEFAULT NULL,
+  `alive` tinyint(1) DEFAULT NULL,
+  `clan_name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `clan_id` int(11) DEFAULT NULL,
+  `gender` int(11) DEFAULT NULL,
+  `race` int(11) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  `exp` int(11) DEFAULT NULL,
+  `power` int(11) DEFAULT NULL,
+  `companion` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+  `peacefulness` int(11) DEFAULT NULL,
+  `peacefulness_verbose` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+  `honor` int(11) DEFAULT NULL,
+  `honor_verbose` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `strength` int(11) DEFAULT NULL,
+  `physic` int(11) DEFAULT NULL,
+  `magic` int(11) DEFAULT NULL,
+  `lvl_equip` int(11) DEFAULT NULL,
+  `lvl_equip_title` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `avg_equip` int(11) DEFAULT NULL,
+  `speed` float DEFAULT NULL,
+  `initiative` float DEFAULT NULL,
+  `position` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+  `quest` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `quest_list` varchar(500) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы masters
+-- Структура таблицы `masters`
 --
 
-CREATE TABLE masters (
-  id int(11) NOT NULL,
-  name varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  profession int(11) DEFAULT NULL,
-  gender int(11) DEFAULT NULL,
-  race int(11) DEFAULT NULL,
-  practic varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  cosmetic varchar(40) COLLATE utf8_bin DEFAULT NULL,
-  city_id int(11) NOT NULL,
-  building int(11) DEFAULT NULL,
-  integrity float DEFAULT NULL,
-  politic_power int(11) DEFAULT NULL,
-  power_outer int(11) DEFAULT NULL,
-  power_outer_fraction int(11) DEFAULT NULL,
-  power_inner int(11) DEFAULT NULL,
-  power_inner_fraction int(11) DEFAULT NULL,
-  job_effect int(11) DEFAULT NULL,
-  job_name varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  positive_job int(11) DEFAULT NULL,
-  negative_job int(11) DEFAULT NULL
+CREATE TABLE `masters` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+  `profession` int(11) DEFAULT NULL,
+  `gender` int(11) DEFAULT NULL,
+  `race` int(11) DEFAULT NULL,
+  `practic` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+  `cosmetic` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+  `city_id` int(11) DEFAULT NULL,
+  `building` int(11) DEFAULT NULL,
+  `integrity` float DEFAULT NULL,
+  `politic_power` int(11) DEFAULT NULL,
+  `power_outer` int(11) DEFAULT NULL,
+  `power_outer_fraction` int(11) DEFAULT NULL,
+  `power_inner` int(11) DEFAULT NULL,
+  `power_inner_fraction` int(11) DEFAULT NULL,
+  `job_effect` int(11) DEFAULT NULL,
+  `job_name` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `positive_job` int(11) DEFAULT NULL,
+  `negative_job` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы places
+-- Структура таблицы `places`
 --
 
-CREATE TABLE places (
-  id int(11) NOT NULL,
-  name varchar(40) COLLATE utf8_bin NOT NULL,
-  frontier tinyint(1) DEFAULT NULL,
-  specialization int(11) DEFAULT NULL,
-  demographics varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  demographics_title varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  persons varchar(500) COLLATE utf8_bin DEFAULT NULL,
-  stability float DEFAULT NULL,
-  stability_title varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  freedom float DEFAULT NULL,
-  freedom_title varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  production int(11) DEFAULT NULL,
-  production_title varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  transport float DEFAULT NULL,
-  transport_title varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  safety float DEFAULT NULL,
-  safety_title varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  culture float DEFAULT NULL,
-  culture_title varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  area int(11) DEFAULT NULL,
-  size int(11) DEFAULT NULL,
-  economy int(11) DEFAULT NULL,
-  politic_power float DEFAULT NULL,
-  power_outer int(11) DEFAULT NULL,
-  power_outer_fraction float DEFAULT NULL,
-  power_inner int(11) DEFAULT NULL,
-  power_inner_fraction float DEFAULT NULL,
-  job_effect int(11) DEFAULT NULL,
-  job_name varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  positive_job int(11) DEFAULT NULL,
-  negative_job int(11) DEFAULT NULL
+CREATE TABLE `places` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) COLLATE utf8_bin NOT NULL,
+  `frontier` tinyint(1) DEFAULT NULL,
+  `specialization` int(11) DEFAULT NULL,
+  `demographics` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `demographics_title` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `persons` varchar(500) COLLATE utf8_bin DEFAULT NULL,
+  `stability` float DEFAULT NULL,
+  `stability_title` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `freedom` float DEFAULT NULL,
+  `freedom_title` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `production` int(11) DEFAULT NULL,
+  `production_title` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `transport` float DEFAULT NULL,
+  `transport_title` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `safety` float DEFAULT NULL,
+  `safety_title` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `culture` float DEFAULT NULL,
+  `culture_title` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `area` int(11) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `economy` int(11) DEFAULT NULL,
+  `politic_power` float DEFAULT NULL,
+  `power_outer` int(11) DEFAULT NULL,
+  `power_outer_fraction` float DEFAULT NULL,
+  `power_inner` int(11) DEFAULT NULL,
+  `power_inner_fraction` float DEFAULT NULL,
+  `job_effect` int(11) DEFAULT NULL,
+  `job_name` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `positive_job` int(11) DEFAULT NULL,
+  `negative_job` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы table_updates
+-- Структура таблицы `table_updates`
 --
 
-CREATE TABLE table_updates (
-  table_name varchar(40) COLLATE utf8_bin NOT NULL,
-  updated_at datetime DEFAULT NULL
+CREATE TABLE `table_updates` (
+  `table_name` varchar(40) COLLATE utf8_bin NOT NULL,
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -144,28 +146,29 @@ CREATE TABLE table_updates (
 --
 
 --
--- Индексы таблицы heroes
+-- Индексы таблицы `heroes`
 --
-ALTER TABLE heroes
-  ADD PRIMARY KEY (id);
+ALTER TABLE `heroes`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы masters
+-- Индексы таблицы `masters`
 --
-ALTER TABLE masters
-  ADD PRIMARY KEY (id);
+ALTER TABLE `masters`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы places
+-- Индексы таблицы `places`
 --
-ALTER TABLE places
-  ADD PRIMARY KEY (id);
+ALTER TABLE `places`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы table_updates
+-- Индексы таблицы `table_updates`
 --
-ALTER TABLE table_updates
-  ADD PRIMARY KEY (table_name);
+ALTER TABLE `table_updates`
+  ADD PRIMARY KEY (`table_name`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
