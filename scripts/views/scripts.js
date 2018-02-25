2a02fc668db31adaa43c7f57350702e46c97e558
MaksymMaksymov <maksymmaksymov@gmail.com>
2018-02-25 12:21:51 +0200

    deleted js script

--- scripts/views/scripts.js
+++
@@ -1,7 +0,0 @@
-$( window ).scroll(function() {
-    if ($(window).scrollTop() > 50) {
-        $('.data_table').addClass('fixTableHead');
-    } else {
-        $('.data_table').removeClass('fixTableHead');
-    }
-});
