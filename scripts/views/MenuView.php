6cb0e2e4ff420b27137776b3eeed348851030e6e
MaksymMaksymov <maksymmaksymov@gmail.com>
2019-12-05 23:46:07 +0200

    Menu updated

--- scripts/views/MenuView.php
+++ scripts/views/MenuView.php
@@ -12,12 +12,14 @@
 				<li onclick="getAction('main_form')"><a href="#">Главная</a></li>
 				<li onclick="getAction('places_form')"><a href="#">Города</a></li>
 				<li onclick="getAction('masters_form')"><a href="#">Мастера</a></li>
+				<li onclick="getAction('emissaries_form')"><a href="#">Эмиссары</a></li>
 				<li onclick="getAction('heroes_form')"><a href="#">Герои</a></li>
 				<li onclick="getAction('jobs_form')"><a href="#">Проекты</a></li>
 				<li onclick="getAction('voices_form')"><a href="#">Голоса</a></li>
 				<form id = "main_form" action="MainController.php" method="POST"></form>
 				<form id = "places_form" action="PlacesController.php" method="POST"></form>
 				<form id = "masters_form" action="MastersController.php" method="POST"></form>
+				<form id = "emissaries_form" action="EmissariesController.php" method="POST"></form>
 				<form id = "heroes_form" action="HeroesController.php" method="POST"></form>
 				<form id = "jobs_form" action="JobsController.php" method="POST"></form>
 				<form id = "voices_form" action="VoicesController.php" method="POST"></form>
